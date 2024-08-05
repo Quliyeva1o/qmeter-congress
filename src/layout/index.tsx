1fd9c9f1fd861763819beb1a6cde2eeda1f3478e
Quliyeva1o <renaquliyeva646@gmail.com>
2024-08-05 15:47:36 +0400

    .

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from './index.module.scss'
+import Header from "./header";
 
 const Layout = () => {
   return (
       <div className={styles.layout}>
+        <Header/>
         <Outlet />
       </div>
   );
