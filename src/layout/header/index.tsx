e2814f67bcb3188f65e12be442f9b611402343ec
a1ishco <ali.bashirov02@gmail.com>
2024-08-05 15:45:21 +0400

    chore: Remove unnecessary code in layout component

---
+++ src/layout/header/index.tsx
@@ -0,0 +1,11 @@
+import Logo from '../../assets/logo/logo'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Logo />
+
+  )
+}
+
+export default Header
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Logo from "../assets/logo/logo";
 import { Outlet } from "react-router-dom";
 import styles from './index.module.scss'
 
 const Layout = () => {
   return (
       <div className={styles.layout}>
-        <Logo />
         <Outlet />
       </div>
   );
