ff1350ed76141b6fecb31bcdd7f1d0ebf414cafc
a1ishco <ali.bashirov02@gmail.com>
2024-08-05 15:42:25 +0400

    Refactor UserTypes interface in index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,10 @@ export  interface UserTypes {
   phone: string;
   email: string;
 }
-
-
 export interface QuestionTypes {
     from: string;
     to: string;
     question: string;
     count: number;
-    liked: boolean; }
+    liked: boolean; 
+  }
