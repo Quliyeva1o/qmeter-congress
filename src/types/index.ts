c4f037c4a3861f75302151726f8d9a7b6b7da184
Quliyeva1o <renaquliyeva646@gmail.com>
2024-08-05 15:40:54 +0400

    .

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,19 @@
-export  interface UserTypes {
+export interface UserTypes {
   name: string;
   phone: string;
   email: string;
 }
 
-
 export interface QuestionTypes {
-    from: string;
-    to: string;
-    question: string;
-    count: number;
-    liked: boolean; }
+  from: string;
+  to: string;
+  question: string;
+  count: number;
+  liked: boolean;
+}
+
+export interface FormOption {
+  name: keyof UserTypes;
+  placeholder: string;
+  rules: any[];
+}
